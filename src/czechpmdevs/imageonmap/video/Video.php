0a3a6d54029b8b28c650333d78ad23add05f429e
NuhDev <92098247+NuhDevX@users.noreply.github.com>
2024-12-07 21:26:03 +0700

    Create Video.php

---
+++ src/czechpmdevs/imageonmap/video/Video.php
@@ -0,0 +1,38 @@
+<?php
+namespace czechpmdevs\imageonmap\video;
+
+use czechpmdevs\imageonmap\util\CustomMapItemDataPacket;
+
+
+/**
+ * Class Video
+ * @package Jibix\MapVideo\video
+ * @author Jibix
+ * @date 01.12.2023 - 14:51
+ * @project MapVideo
+ */
+class Video{
+
+    public static function id(string $name): int{
+        return crc32(md5($name));
+    }
+
+    /**
+     * Video constructor.
+     * @param int $id
+     * @param CustomMapItemDataPacket[] $frames
+     */
+    public function __construct(protected int $id, protected array $frames){}
+
+    public function getId(): int{
+        return $this->id;
+    }
+
+    public function getFrames(): array{
+        return $this->frames;
+    }
+
+    public function getFrame(int $index): ?CustomMapItemDataPacket{
+        return $this->frames[$index] ?? null;
+    }
+}
