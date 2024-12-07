641f13a52dd66bcb6d0f31e4d9c2be107cb20435
NuhDev <92098247+NuhDevX@users.noreply.github.com>
2024-12-07 21:39:31 +0700

    Create LoadVideoAsyncTask.php

---
+++ src/czechpmdevs/imageonmap/task/LoadVideoAsyncTask.php
@@ -0,0 +1,53 @@
+<?php
+namespace czechpmdevs\imageonmap\task;
+use Closure;
+use czechpmdevs\imageonmap\utils\CustomMapItemDataPacket;
+use czechpmdevs\imageonmap\utils\Utils;
+use czechpmdevs\imageonmap\video\Video;
+use czechpmdevs\imageonmap\video\VideoManager;
+use pmmp\thread\ThreadSafeArray;
+use pocketmine\scheduler\AsyncTask;
+
+
+/**
+ * Class LoadVideoAsyncTask
+ * @package Jibix\MapVideo\task
+ * @author Jibix
+ * @date 01.12.2023 - 14:54
+ * @project MapVideo
+ */
+class LoadVideoAsyncTask extends AsyncTask{
+
+    public function __construct(
+        private int $id,
+        private string $file,
+        private bool $cache,
+        private Closure $onComplete,
+        private Closure $progressNotifier,
+    ){}
+
+    public function onRun(): void{
+        $frames = new ThreadSafeArray();
+        $totalFrames = count($videoFrames = Utils::videoToFrames($this->file));
+        foreach ($videoFrames as $i => $frame) {
+            $frames[] = Utils::frameToColors(Utils::frameToImage($frame));
+            if ($this->progressNotifier !== null) $this->publishProgress([$totalFrames, $i +1]);
+        }
+        $this->setResult($frames);
+    }
+
+    public function onCompletion(): void{
+        $packets = [];
+        foreach ($this->getResult() as $frame) {
+            $packets[] = CustomMapItemDataPacket::create($this->id, $frame);
+        }
+        ($this->onComplete)($video = new Video($this->id, $packets));
+        if ($this->cache) VideoManager::getInstance()->cacheVideo($video);
+    }
+
+    public function onProgressUpdate($progress): void{
+        //progressNotifier can't be null at this point
+        [$totalFrames, $loadedFrames] = $progress;
+        ($this->progressNotifier)($totalFrames, $loadedFrames);
+    }
+}
